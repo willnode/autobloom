7e7205f165b68df776094f1ed03423d44cfbbc66
Aritz T <59333567+Aritz331@users.noreply.github.com>
2022-06-23 19:22:03 +0200

    Disable on-screen controls

--- src/run.bat
+++ src/run.bat
@@ -4,7 +4,7 @@ cd /D "%~dp0"
 cd ..\mpv
 %home%ChangeWallpaper.exe %home%..\media\first.bmp
 timeout 1
-start /B mpv %home%..\media\bloom.mp4
+start /B mpv %home%..\media\bloom.mp4 --no-osc
 cd ..\src
 timeout 5
 %home%ChangeWallpaper.exe %home%..\media\last.bmp
