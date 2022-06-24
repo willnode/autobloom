fc982fc5cf53d3b9d5c3ce7e88ee87902926bbd3
Aritz T <59333567+Aritz331@users.noreply.github.com>
2022-06-24 17:30:36 +0200

    Not very important changes, just little improvements

--- uninstall.bat
+++ uninstall.bat
@@ -1,8 +1,8 @@
 @echo off
 if not "%1"=="am_admin" (powershell start -verb runas '%0' am_admin & exit /b)
-
 SCHTASKS /DELETE /TN "AUTOBLOOM" /f
 SCHTASKS /DELETE /TN "AUTOBLOOM PREP" /f
+cls
 
-echo "AutoBloom has been removed."
-timeout 3
+echo AutoBloom has been removed.
+timeout 3 >nul
