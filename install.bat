7de4d2e57cf17a64301f20c83b9f25142af850ac
Aritz T <59333567+Aritz331@users.noreply.github.com>
2022-06-24 17:28:25 +0200

    Not very important changes, just little improvements

--- install.bat
+++ install.bat
@@ -1,8 +1,8 @@
 @echo off
 if not "%1"=="am_admin" (powershell start -verb runas '%0' am_admin & exit /b)
-echo %USERNAME%
 SCHTASKS /CREATE /SC ONLOGON /TN "AUTOBLOOM" /TR "%~dp0src\RunSilent.exe"
 SCHTASKS /CREATE /SC ONSTART /TN "AUTOBLOOM PREP" /TR "%~dp0src\off.bat"
+cls
 
-echo "AutoBloom has been installed!"
-timeout 3
+echo AutoBloom has been installed!
+timeout 3 >nul
