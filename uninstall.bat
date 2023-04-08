be77c8756de65b1ec99e4fb22202f3de268971f6
Aritz T <59333567+aritz331@users.noreply.github.com>
2023-04-08 23:39:12 +0200

    aborts if not admin

--- uninstall.bat
+++ uninstall.bat
@@ -1,5 +1,6 @@
 @echo off
 if not "%1"=="am_admin" (powershell start -verb runas '%0' am_admin & exit /b)
+dism || exit
 SCHTASKS /DELETE /TN "AUTOBLOOM" /f
 SCHTASKS /DELETE /TN "AUTOBLOOM PREP" /f
 cls
