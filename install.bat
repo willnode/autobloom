dd981e4a68155f0fe60e89319891cd05ddee0882
Aritz T <59333567+aritz331@users.noreply.github.com>
2023-04-08 23:38:15 +0200

    aborts if not admin

--- install.bat
+++ install.bat
@@ -1,5 +1,6 @@
 @echo off
 if not "%1"=="am_admin" (powershell start -verb runas '%0' am_admin & exit /b)
+dism || exit
 SCHTASKS /CREATE /SC ONLOGON /TN "AUTOBLOOM" /TR "%~dp0src\run.bat"
 SCHTASKS /CREATE /SC ONSTART /TN "AUTOBLOOM PREP" /TR "%~dp0src\prep.bat"
 cls
