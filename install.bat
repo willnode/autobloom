7878642eee64e1c058c7cb950599343bcf539c8c
Aritz T <59333567+aritz331@users.noreply.github.com>
2023-04-08 23:36:51 +0200

    final change, adapting to exe removal and off.bat rename

--- install.bat
+++ install.bat
@@ -1,7 +1,7 @@
 @echo off
 if not "%1"=="am_admin" (powershell start -verb runas '%0' am_admin & exit /b)
-SCHTASKS /CREATE /SC ONLOGON /TN "AUTOBLOOM" /TR "%~dp0src\RunSilent.exe"
-SCHTASKS /CREATE /SC ONSTART /TN "AUTOBLOOM PREP" /TR "%~dp0src\off.bat"
+SCHTASKS /CREATE /SC ONLOGON /TN "AUTOBLOOM" /TR "%~dp0src\run.bat"
+SCHTASKS /CREATE /SC ONSTART /TN "AUTOBLOOM PREP" /TR "%~dp0src\prep.bat"
 cls
 
 echo AutoBloom has been installed!
