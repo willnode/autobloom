331812fd4fcf6d61b4b0242842cbf76bd35fae77
Aritz T <59333567+aritz331@users.noreply.github.com>
2023-04-09 18:10:16 +0200

    fixed dumb error (prep.bat) lol

--- install.bat
+++ install.bat
@@ -1,7 +1,7 @@
 @echo off
 if not "%1"=="am_admin" (powershell start -verb runas '%0' am_admin & exit /b)
-SCHTASKS /CREATE /SC ONLOGON /TN "AUTOBLOOM" /TR "wscript '%~dp0src\silent.vbs' 'conhost cmd /c '%~dp0src\run.bat' ch'"
-SCHTASKS /CREATE /SC ONSTART /TN "AUTOBLOOM PREP" /TR "wscript '%~dp0src\silent.vbs' 'conhost cmd /c '%~dp0src\prep.bat''"
+SCHTASKS /CREATE /SC ONLOGON /TN "AUTOBLOOM"      /TR "wscript '%~dp0src\silent.vbs' 'conhost cmd /c '%~dp0src\run.bat' ch'"
+SCHTASKS /CREATE /SC ONSTART /TN "AUTOBLOOM PREP" /TR "wscript '%~dp0src\silent.vbs' 'conhost cmd /c '%~dp0src\prep.bat'  '"
 cls
 
 echo AutoBloom has been installed!
