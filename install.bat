b4edfb2d5d99c94c845dfd2bd9810ed9b242832b
Aritz T <59333567+aritz331@users.noreply.github.com>
2023-04-09 17:56:23 +0200

    add vbs silent

--- install.bat
+++ install.bat
@@ -1,9 +1,9 @@
 @echo off
 if not "%1"=="am_admin" (powershell start -verb runas '%0' am_admin & exit /b)
-dism || exit
-SCHTASKS /CREATE /SC ONLOGON /TN "AUTOBLOOM" /TR "%~dp0src\run.bat"
-SCHTASKS /CREATE /SC ONSTART /TN "AUTOBLOOM PREP" /TR "%~dp0src\prep.bat"
+SCHTASKS /CREATE /SC ONLOGON /TN "AUTOBLOOM" /TR "cscript //NOLOGO '%~dp0src\silent.vbs' 'conhost cmd /c '%~dp0src\run.bat' ch'"
+SCHTASKS /CREATE /SC ONSTART /TN "AUTOBLOOM PREP" /TR "cscript //NOLOGO '%~dp0src\silent.vbs' 'conhost cmd /c '%~dp0src\prep.bat''"
 cls
 
 echo AutoBloom has been installed!
+
 timeout 3 >nul
--- src/prep.bat
+++ src/prep.bat
@@ -1,3 +1,3 @@
 @echo off
 set "_home=%~dp0"
-powershell -NoP .\wallpaper.ps1 "%_home%..\media\last.bmp"
+powershell -NoP .\wallpaper.ps1 "%_home%..\media\first.bmp"
