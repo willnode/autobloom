950b6c32874303a4c1cfc4a71badb1a98a057d89
Aritz T <59333567+aritz331@users.noreply.github.com>
2023-04-18 19:13:29 +0200

    add dark theme + smaller files & higher quality

--- install.bat
+++ install.bat
@@ -1,7 +1,13 @@
 @echo off
 if not "%1"=="am_admin" (powershell start -verb runas '%0' am_admin & exit /b)
-SCHTASKS /CREATE /SC ONLOGON /TN "AUTOBLOOM"      /TR "wscript '%~dp0src\silent.vbs' 'conhost cmd /c '%~dp0src\run.bat' ch'"
-SCHTASKS /CREATE /SC ONSTART /TN "AUTOBLOOM PREP" /TR "wscript '%~dp0src\silent.vbs' 'conhost cmd /c '%~dp0src\prep.bat'  '" /RU "SYSTEM"
+
+echo Select theme:
+dir /b media
+echo.
+set /p "_theme=> "
+
+SCHTASKS /CREATE /SC ONLOGON /TN "AUTOBLOOM"      /TR "wscript '%~dp0src\silent.vbs' 'conhost cmd /c '%~dp0src\run.bat' ch %_theme% '"
+SCHTASKS /CREATE /SC ONSTART /TN "AUTOBLOOM PREP" /TR "wscript '%~dp0src\silent.vbs' 'conhost cmd /c '%~dp0src\prep.bat' %_theme% '" /RU "SYSTEM"
 powershell -Command "Set-ExecutionPolicy -Scope CurrentUser -ExecutionPolicy RemoteSigned"
 cls
 
--- src/run.bat
+++ src/run.bat
@@ -1,14 +1,16 @@
 @echo off
 if not [%1]==[ch] (start conhost cmd /c "" "%~dpnx0" ch & exit)
+shift
+set "_theme=%~1"
 powershell -NoP -W hidden ; exit
 set "_home=%~dp0"
 cd /D "%_home%"
 
-powershell -NoP .\wallpaper.ps1 "%_home%..\media\first.bmp"
+powershell -NoP .\wallpaper.ps1 "%_home%..\media\%_theme%\first.webp"
 
 timeout 1
-start /B ..\mpv\mpv ..\media\bloom.mp4 --no-osc --no-input-default-bindings --no-taskbar-progress --no-window-dragging ^
+start /B ..\mpv\mpv ..\media\%_theme%\bloom.mp4 --no-osc --no-input-default-bindings --no-taskbar-progress --no-window-dragging ^
     --deband --hwdec=auto --no-border --cache=yes --demuxer-max-bytes=488281KiB --demuxer-readahead-secs=120 --autofit=100%% --player-operation-mode=pseudo-gui --input-ipc-server=\\.\pipe\mpvsocket --force-window=yes --volume=0
 cd ..\src
 timeout 2
-powershell -NoP .\wallpaper.ps1 "%_home%..\media\last.bmp"
+powershell -NoP .\wallpaper.ps1 "%_home%..\media\%_theme%\last.webp"
