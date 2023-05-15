48542aef98a0f64d172ed4909a5cd282d24b38fb
Aritz T <59333567+aritz331@users.noreply.github.com>
2023-05-15 11:38:40 +0200

    disable even more bindings

--- src/run.bat
+++ src/run.bat
@@ -9,7 +9,7 @@ cd /D "%_home%"
 powershell -NoP .\wallpaper.ps1 "%_home%..\media\%_theme%\first.webp"
 
 timeout 1
-start /B ..\mpv\mpv ..\media\%_theme%\bloom.mp4 --no-osc --no-input-default-bindings --no-taskbar-progress --no-window-dragging ^
+start /B ..\mpv\mpv ..\media\%_theme%\bloom.mp4 --no-osc --no-input-default-bindings --no-input-builtin-bindings --no-taskbar-progress --no-window-dragging ^
     --deband --hwdec=auto --no-border --cache=yes --demuxer-max-bytes=488281KiB --demuxer-readahead-secs=120 --autofit=100%% --player-operation-mode=pseudo-gui --input-ipc-server=\\.\pipe\mpvsocket --force-window=yes --volume=0
 cd ..\src
 timeout 2
