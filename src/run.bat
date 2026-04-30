8fcf9cee0993a5bc6a8a8b35342883e360401370
trevi <aritz331@gmail.com>
2026-04-30 17:00:28 +0200

    perf: slight optimization for video playback

--- src/run.bat
+++ src/run.bat
@@ -1,11 +1,11 @@
-rem @echo off
+@echo off
 set "_home=%~dp0"
 cd /D "%_home%"
 set "_theme=%~1"
 powershell -NoP -ExecutionPolicy UnRestricted .\wallpaper.ps1 \"%_home%..\media\%_theme%\first.png\"
 
 timeout 1
-start /B ..\mpv\mpv ..\media\%_theme%\bloom.mp4 --volume=0 --wid=0 --force-window=yes --no-window-dragging --cursor-autohide=no --stop-screensaver=no --input-default-bindings=no --keepaspect=no --no-osc  --hwdec=no --no-config
+start /min wscript silent.vbs ..\mpv\mpv ..\media\%_theme%\bloom.mp4 --volume=0 --wid=0 --force-window=yes --no-window-dragging --cursor-autohide=no --stop-screensaver=no --input-default-bindings=no --keepaspect=no --no-osc --hwdec=auto --gpu-api=d3d11 --profile=high-quality --deband=no --no-config
 timeout 2
 powershell -NoP -ExecutionPolicy UnRestricted .\wallpaper.ps1 \"%_home%..\media\%_theme%\last.png\"
-exit
+exit /b 0
