7251d3cce20d88d9e9be95aaf04a8ed6f372283c
trevi <aritz331@gmail.com>
2026-04-30 17:04:35 +0200

    style(wallpaper.ps1): indentation/whitespace tweaks

--- src/wallpaper.ps1
+++ src/wallpaper.ps1
@@ -1,20 +1,18 @@
 $imgPath = $args[0]
 $code = @' 
 using System.Runtime.InteropServices; 
-namespace Win32{ 
-    
-     public class Wallpaper{ 
-        [DllImport("user32.dll", CharSet=CharSet.Auto)] 
-         static extern int SystemParametersInfo (int uAction , int uParam , string lpvParam , int fuWinIni) ; 
-         
-         public static void SetWallpaper(string thePath){ 
-            SystemParametersInfo(20,0,thePath,3); 
-         }
+namespace Win32 {
+    public class Wallpaper {
+        [DllImport("user32.dll", CharSet=CharSet.Auto)]
+        static extern int SystemParametersInfo (int uAction, int uParam, string lpvParam, int fuWinIni);
+
+        public static void SetWallpaper(string filePath) {
+		    SystemParametersInfo(20, 0, filePath, 3);
+        }
     }
- } 
+}
 '@
 
-add-type $code 
+add-type $code
 
-#Apply the Change on the system 
 [Win32.Wallpaper]::SetWallpaper($imgPath)
